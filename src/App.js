605d1df9956652907aed5230611c3a2eaf80d60d
d.beltran <bleachstick@hotmail.com>
2019-03-13 06:41:00 -0500

    Drawer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Login} from "./components/Login";
+import ResponsiveDrawer from "./components/Drawer";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
             <Router>
                 <div className="App">
                     <Route exact path="/" component={Login}/>
+                    <Route path="/main" component={ResponsiveDrawer}/>
                 </div>
             </Router>
         );
---
+++ src/components/Drawer.js
@@ -0,0 +1,181 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+import EventIcon from '@material-ui/icons/Event';
+import EditIcon from '@material-ui/icons/Edit';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import MenuIcon from '@material-ui/icons/Menu';
+import StarIcon from '@material-ui/icons/Star';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import ViewQuiltIcon from '@material-ui/icons/ViewQuilt';
+import { withStyles } from '@material-ui/core/styles';
+
+import {Link} from 'react-router-dom';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    appBar: {
+        marginLeft: drawerWidth,
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+    },
+    menuButton: {
+        marginRight: 20,
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    toolbar: {
+        marginTop: 3
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+    },
+    orangeAvatar: {
+        margin: 1,
+        color: '#fff',
+        backgroundColor: deepOrange[500],
+    },
+    icon: {
+        marginLeft: 104,
+    },
+    logout: {
+        marginTop: "55vh",
+    },
+});
+
+class ResponsiveDrawer extends React.Component {
+    state = {
+        mobileOpen: false,
+    };
+
+    handleDrawerToggle = () => {
+        this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+    };
+
+    render() {
+        const { classes, theme } = this.props;
+
+        const drawer = (
+            <div>
+                <div className={classes.toolbar} />
+                <ListItem>
+                    <Avatar className={classes.orangeAvatar}>A</Avatar>
+                    <ListItemText primary="admin" secondary="admin@gmail.com" />
+                    <IconButton aria-label="Delete" className={classes.icon}>
+                        <EditIcon fontSize="small"/>
+                    </IconButton>
+                </ListItem>
+                <Divider />
+                <ListItem button key="Events" component={Link} to="">
+                    <ListItemIcon>
+                        <ViewQuiltIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Events" />
+                </ListItem>
+                <ListItem button key="MyEvents" component={Link} to="">
+                    <ListItemIcon>
+                        <StarIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="My Events" />
+                </ListItem>
+                <ListItem button key="Calendar" component={Link} to="">
+                    <ListItemIcon>
+                        <EventIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Calendar" />
+                </ListItem>
+                <Divider />
+                <ListItem button key="Logout" component={Link} to="">
+                    <ListItemIcon>
+                        <MeetingRoomIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Logout" />
+                </ListItem>
+            </div>
+        );
+
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerToggle}
+                            className={classes.menuButton}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Events
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <nav className={classes.drawer}>
+                    {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                    <Hidden smUp implementation="css">
+                        <SwipeableDrawer
+                            container={this.props.container}
+                            variant="temporary"
+                            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                            open={this.state.mobileOpen}
+                            onClose={this.handleDrawerToggle}
+                            onOpen={this.handleDrawerToggle} // only included for warning in devtools console
+                            classes={{
+                                paper: classes.drawerPaper,
+                            }}
+                        >
+                            {drawer}
+                        </SwipeableDrawer>
+                    </Hidden>
+                    <Hidden xsDown implementation="css">
+                        <Drawer
+                            classes={{
+                                paper: classes.drawerPaper,
+                            }}
+                            variant="permanent"
+                            open
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                </nav>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+
+                </main>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(ResponsiveDrawer);
